543c591610b05f8a8d5ec3da4483a6dd1b121b69
himani_singh <singhhimani685@gmail.com>
2023-10-09 19:48:53 +0530

    checkprime code

---
+++ checkprime.js
@@ -0,0 +1,14 @@
+function checkPrime(n){
+    let count = 0 
+    for( let i = 1 ; i <= n ; i++){
+        if(n%i == 0){
+        count = count + 1
+    }}
+    if(count<=1){
+        console.log("prime")
+    }else{
+        console.log("not prime")
+    }
+}
+
+checkPrime(5)
